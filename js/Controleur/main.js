15d5322a92f34e7fb6a9c33f8c113b42d88d3127
C M <clement.michaud@etu.univ-grenoble-alpes.fr>
2026-03-11 18:09:41 +0100

    Visuel

--- js/Controleur/main.js
+++ js/Controleur/main.js
@@ -1,1 +1,18 @@
+const input = document.getElementById('pokemonInput');
+const searchBtn = document.getElementById('searchBtn');
+const favBtn = document.getElementById('favBtn');
 
+// On écoute chaque touche tapée dans le champ
+input.addEventListener('input', () => {
+    const value = input.value.trim();
+    
+    // Si le champ n'est pas vide, on active les boutons 
+    if (value.length > 0) {
+        searchBtn.disabled = false;
+        favBtn.disabled = false;
+    } else {
+        // Sinon, on les bloque
+        searchBtn.disabled = true;
+        favBtn.disabled = true;
+    }
+});
