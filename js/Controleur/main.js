7125c5df1fb217a5f1681b93af666c1287497d88
collero <robin.colle@etu.univ-grenoble-alpes.fr>
2026-03-13 11:45:56 +0100

    essai du premier commir

--- js/Controleur/main.js
+++ js/Controleur/main.js
@@ -1,3 +1,6 @@
+import * as UserModel from '../models/userModel.js';
+
+
 const input = document.getElementById('pokemonInput');
 const searchBtn = document.getElementById('searchBtn');
 const favBtn = document.getElementById('favBtn');
@@ -16,3 +19,45 @@ input.addEventListener('input', () => {
         favBtn.disabled = true;
     }
 });
+
+
+
+input.addEventListener('input', () => {
+    const value = input.value.trim();
+    
+    const getRikishiData = async () => {
+  const url = 'https://www.sumo-api.com/api/rikishis';
+
+  try {
+    const response = await fetch(url);
+
+    // On vérifie si la réponse est correcte (status 200-299)
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
+    }
+
+    const data = await response.json();
+    
+    // On affiche les données dans la console pour voir la structure
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Impossible de récupérer les données :", error);
+  }
+};
+
+// Appel de la fonction
+getRikishiData();
+});
+
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const user = await UserModel.findUserById(req.params.id);
+        if (!user) return res.status(404).json({ message: "Utilisateur introuvable" });
+        
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Erreur serveur" });
+    }
+};
--- js/Model/Pokemon.js
+++ js/Model/Pokemon.js
@@ -1,1 +1,36 @@
 
+const getRikishiData = async () => {
+  const url = 'https://www.sumo-api.com/api/rikishis';
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
+    }
+
+    const data = await response.json();
+    
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Impossible de récupérer les données :", error);
+  }
+};
+getRikishiData();
+
+
+export const findUserById = async (id) => {
+    // On ne gère que la base de données ici
+    return await db.query("SELECT * FROM users WHERE id = ?", [id]);
+};
+
+/*
+https://www.sumo-api.com/api/rikishis?shikonaEn=Asanoyama
+https://www.sumo-api.com/api/rikishis?heya=Yamahibiki
+
+
+
+
+
+*/
